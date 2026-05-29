b141c6b2654a75ee377f718a42dd28359345fe8b
ruksat13 <mdruksathasanakib@gmail.com>
2026-05-29 09:50:55 +0600

    Fix profile link in admin sidebar

--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -44,6 +44,7 @@ const Sidebar = () => {
             <NavLink to="/dashboard/all-users" className={linkClass}>👥 All Users</NavLink>
             <NavLink to="/dashboard/all-blood-donation-request" className={linkClass}>🩸 All Donation Requests</NavLink>
             <NavLink to="/dashboard/profile" className={linkClass}>👤 Profile</NavLink>
+            <NavLink to="/dashboard/profile" className={linkClass}>👤 Profile</NavLink>
         </>
     );
 
